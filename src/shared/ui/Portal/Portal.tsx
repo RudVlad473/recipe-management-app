7ab14ea86e94e0d04e780691623f75e20dcdd203
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-06-13 13:15:32 +0300

    add portal

--- src/shared/ui/Portal/Portal.tsx
+++ src/shared/ui/Portal/Portal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, PropsWithChildren } from "react"
+import { FC, PropsWithChildren } from "react"
 import { createPortal } from "react-dom"
 
 export const Portal: FC<Required<PropsWithChildren>> = ({ children }) => {
