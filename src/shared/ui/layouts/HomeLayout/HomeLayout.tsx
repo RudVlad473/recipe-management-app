98be162328b42a7dbe17aca966735056cc2572f9
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-06-13 13:14:08 +0300

    add home layout

--- src/shared/ui/layouts/HomeLayout/HomeLayout.tsx
+++ src/shared/ui/layouts/HomeLayout/HomeLayout.tsx
@@ -1,27 +1,51 @@
+import { useAppDispatch } from "../../../../app/lib"
+import { appendFilters, dropFilters } from "../../../../entities/Recipe/model/filtersSlice"
+import { Details } from "../../../../features/Details/ui/Details"
 import { SearchBar } from "../../../../features/SearchBar/ui"
 import styles from "./HomeLayout.module.scss"
-import React, { FC, PropsWithChildren } from "react"
+import { FC, PropsWithChildren, useCallback } from "react"
 
 export const HomeLayout: FC<PropsWithChildren> = ({ children }) => {
+  const dispatch = useAppDispatch()
+
+  const handleOnSearch = useCallback(
+    (value: string) => {
+      dispatch(
+        appendFilters({
+          title: value,
+        })
+      )
+    },
+    [dispatch]
+  )
+
+  const handleOnChange = useCallback(
+    (value: string) => {
+      if (value === "") {
+        dispatch(
+          dropFilters({
+            title: true,
+          })
+        )
+      }
+    },
+    [dispatch]
+  )
+
   return (
     <main className={styles["home-layout"]}>
-      {/* <div
-        className={styles.home}
-        onClick={() => navigate(HomeRoutes.HOME)}>
-        <HomeOutlined style={{ fontSize: "200%" }} />
-      </div> */}
       <div className={styles["search-bar"]}>
         <SearchBar
-          // onSearch={handleOnSearch}
-          // onChange={handleOnChange}
+          onSearch={handleOnSearch}
+          onChange={handleOnChange}
           // isLoading={loading}
           allowClear
         />
       </div>
 
-      {/* <div className={styles["details"]}>
+      <div className={styles["details"]}>
         <Details />
-      </div> */}
+      </div>
 
       <section className={styles["children"]}>{children}</section>
     </main>
