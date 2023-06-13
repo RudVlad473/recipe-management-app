4fe43a60e79e5f49ba21e188dc7f27c81a513f43
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-06-13 15:36:18 +0300

    add filter table layout

--- src/widgets/FiltersTable/ui/FiltersTable/FiltersTable.tsx
+++ src/widgets/FiltersTable/ui/FiltersTable/FiltersTable.tsx
@@ -1,6 +1,7 @@
 import { useAppDispatch } from "../../../../app/lib"
 import { appendFilters } from "../../../../entities/Recipe/model/filtersSlice"
 import { timeOptions } from "../../consts"
+import styles from "./FiltersTable.module.scss"
 import { Select } from "antd"
 import { FC } from "react"
 
@@ -8,23 +9,26 @@ export const FiltersTable: FC = () => {
   const dispatch = useAppDispatch()
 
   return (
-    <div>
-      <Select
-        style={{ width: 120 }}
-        onChange={(value, option) => {
-          const time = parseInt(value)
+    <div className={styles["filter-table"]}>
+      <div className={styles.filter}>
+        <h3>Pick time of cooking</h3>
+        <Select
+          style={{ width: 120 }}
+          onChange={(value, option) => {
+            const time = parseInt(value)
 
-          dispatch(
-            appendFilters({
-              cookingTimeMin: time,
-            })
-          )
-        }}
-        options={timeOptions.map((time) => ({
-          value: time,
-          label: `${time} min`,
-        }))}
-      />
+            dispatch(
+              appendFilters({
+                cookingTimeMin: time,
+              })
+            )
+          }}
+          options={timeOptions.map((time) => ({
+            value: time,
+            label: `${time} min`,
+          }))}
+        />
+      </div>
     </div>
   )
 }
