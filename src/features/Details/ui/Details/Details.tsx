50c820e2cec85b5f1d058aa9f88e01e86e349fba
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-06-13 13:13:35 +0300

    fix layout

--- src/features/Details/ui/Details/Details.tsx
+++ src/features/Details/ui/Details/Details.tsx
@@ -1,24 +1,41 @@
+import { useBooleanState } from "../../../../shared/lib/hooks"
 import { Portal } from "../../../../shared/ui/Portal"
 import { Auth } from "../../../../widgets/Auth/ui"
+import { SavedRecipes } from "../../../../widgets/SavedRecipes/ui/SavedRecipes"
 import styles from "./Details.module.scss"
 import { HeartOutlined, UserOutlined } from "@ant-design/icons"
-import React, { FC } from "react"
+import classNames from "classnames"
+import { FC } from "react"
 
 export const Details: FC = () => {
+  const { state: showAuthModal, toggleState: toggleAuthModal } = useBooleanState()
+  const { state: showRecipesModal, toggleState: toggleRecipesModal } = useBooleanState()
+
   return (
     <>
       <div className={styles.details}>
-        <div>
-          <HeartOutlined />
+        <div className={styles.detail} onClick={() => toggleRecipesModal()}>
+          <HeartOutlined style={{ fontSize: "32px" }} />
         </div>
 
-        <div>
-          <UserOutlined />
+        <div className={styles.detail} onClick={() => toggleAuthModal()}>
+          <UserOutlined style={{ fontSize: "32px" }} />
         </div>
       </div>
       <Portal>
-        <div className={styles["auth-modal"]}>
+        <div
+          className={classNames(styles["auth-modal"], {
+            [styles["auth-modal--open"]]: showAuthModal,
+          })}>
           <Auth />
+        </div>
+      </Portal>
+      <Portal>
+        <div
+          className={classNames(styles["recipes-modal"], {
+            [styles["recipes-modal--open"]]: showRecipesModal,
+          })}>
+          <SavedRecipes />
         </div>
       </Portal>
     </>
