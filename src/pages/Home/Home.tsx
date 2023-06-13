864aea2a6e4a1f076685ca111ca119311be5e7c3
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-06-13 11:16:04 +0300

    add home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,20 @@
+import { FC } from "react"
+import recipes from "../../entities/Recipe/lib/data/recipes.json"
+import { RecipeList } from "../../entities/Recipe/ui/RecipeList"
+import { FiltersTable } from "../../widgets/FiltersTable/ui/FiltersTable"
 import styles from "./Home.module.scss"
-import React, { FC } from "react"
 
 export const Home: FC = () => {
   return (
     <div className={styles.content}>
       <aside className={styles.sider}>
-        <h1>Categories</h1>
-        <CategoryMenu disabled={loading} />
+        <h1>Filters</h1>
+        <FiltersTable />
       </aside>
 
-      <div className={styles.main}>{loading ? <Spinner /> : <ItemList items={items} />}</div>
+      <div className={styles.main}>
+        <RecipeList recipes={recipes} />
+      </div>
     </div>
   )
 }
