6fa4475f2dafcd8f3fdba01af8019aa4d0fc9fb1
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-06-13 15:37:57 +0300

    fix layout

--- src/entities/Recipe/ui/RecipeCard/RecipeCard.tsx
+++ src/entities/Recipe/ui/RecipeCard/RecipeCard.tsx
@@ -1,25 +1,21 @@
 import { useAppDispatch, useAppSelector } from "../../../../app/lib"
 import { selectIsLoggedIn } from "../../../User/model"
 import { TRecipeCard } from "../../lib/types"
-import { addRecipe } from "../../model"
+import { addRecipe, selectRecipes } from "../../model"
 import styles from "./RecipeCard.module.scss"
 import { StarOutlined } from "@ant-design/icons"
 import { Image } from "antd"
 import classNames from "classnames"
 import { FC } from "react"
 
-type RecipeCardProps = TRecipeCard & {
-  type?: "CARD" | "COMPACT"
-}
+type RecipeCardProps = TRecipeCard
 
-export const RecipeCard: FC<RecipeCardProps> = ({
-  id,
-  description,
-  title,
-  imageUrl,
-  type = "CARD",
-}) => {
+export const RecipeCard: FC<RecipeCardProps> = ({ id, description, title, imageUrl }) => {
   const dispatch = useAppDispatch()
+
+  const recipes = useAppSelector(selectRecipes)
+
+  const isSaved = !!recipes.find((recipeId) => recipeId.id === id)
 
   const isLoggedIn = useAppSelector(selectIsLoggedIn)
 
@@ -28,31 +24,18 @@ export const RecipeCard: FC<RecipeCardProps> = ({
   }
 
   return (
-    <section
-      className={classNames(
-        styles["recipe"],
-        {
-          [styles["recipe--card"]]: type === "CARD",
-        },
-        {
-          [styles["recipe--compact"]]: type === "COMPACT",
-        }
-      )}>
-      <div className={styles.image}>
-        {
-          <Image
-            style={{ maxWidth: "200px" }}
-            src={imageUrl}
-          />
-        }
-      </div>
+    <section className={classNames(styles["recipe"])}>
+      <div className={styles.image}>{<Image src={imageUrl} />}</div>
       {isLoggedIn && (
         <aside
-          className={styles.star}
+          className={classNames(styles.star, {
+            [styles["star--disabled"]]: isSaved,
+          })}
           onClick={handleAddFavoriteRecipe}>
           <StarOutlined />
         </aside>
       )}
+
       <h2 className={styles.title}>{title}</h2>
       <h3 className={styles.desc}>{description}</h3>
       <footer className={styles.click}>Click to see recipe</footer>
