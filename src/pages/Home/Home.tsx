c8b4af60f0429f6150918b4ff9914556205213d2
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-06-13 15:36:49 +0300

    add delete filters button

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,41 @@
+import { useAppDispatch, useAppSelector } from "../../app/lib"
 import { useFilteredRecipes } from "../../entities/Recipe/lib/hooks"
+import { dropFilters, selectFilters } from "../../entities/Recipe/model/filtersSlice"
 import { RecipeList } from "../../entities/Recipe/ui/RecipeList"
+import { anyValueFull, anyValueTruthy } from "../../shared/lib/utils"
 import { FiltersTable } from "../../widgets/FiltersTable/ui/FiltersTable"
 import styles from "./Home.module.scss"
+import { DeleteOutlined } from "@ant-design/icons"
+import { Tooltip } from "antd"
 import { FC } from "react"
 
 export const Home: FC = () => {
+  const dispatch = useAppDispatch()
+
+  const filters = useAppSelector(selectFilters)
+
+  const anyFilters = anyValueFull(filters)
+
   const { recipes } = useFilteredRecipes()
+
+  function handleDropAllFilters() {
+    dispatch(dropFilters({}))
+  }
 
   return (
     <div className={styles.content}>
       <aside className={styles.sider}>
-        <h1>Filters</h1>
+        <div className={styles["sider__header"]}>
+          <h1>Filters</h1>
+
+          {anyFilters && (
+            <div onClick={handleDropAllFilters}>
+              <Tooltip title={"Drop ALL filters"}>
+                <DeleteOutlined />
+              </Tooltip>
+            </div>
+          )}
+        </div>
         <FiltersTable />
       </aside>
 
